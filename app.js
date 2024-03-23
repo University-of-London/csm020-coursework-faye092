d8c32c5291af88f4b6f0f14a4ec079c8b7fdedab
Feiliao90 <liaofei167@gmail.com>
2024-03-23 10:29:40 +0000

    add routes

--- app.js
+++ app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const app = express();
 const connectDB = require('./database/db');
 const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser');
+const authRoute = require('./routes/auth');
 
 dotenv.config();
+app.use(express.json());
+app.use(cookieParser());
+app.use("/api/auth", authRoute);
 
 
 app.listen(process.env.PORT, () => {
