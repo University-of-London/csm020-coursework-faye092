24e4828cbb33a5db4caf3f915fc0ee767e3b99f6
Feiliao90 <liaofei167@gmail.com>
2024-03-26 21:58:31 +0000

    update reply and reply comment controller

--- routes/comments.js
+++ routes/comments.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {createCommentController, createCommentReplyController} = require("../controllers/commentController");
+const {createCommentController, createCommentReplyController,
+    updateCommentController, updateReplyCommentController} = require("../controllers/commentController");
 
 //CREATE COMMENT
 router.post("/create",createCommentController);
@@ -8,6 +9,10 @@ router.post("/create",createCommentController);
 //CREATE COMMENT REPLY
 router.post("/create/reply/:commentId",createCommentReplyController);
 
+//UPDATE COMMENT
+router.put("/update/:commentId",updateCommentController);
 
+//UPDATE REPLY COMMENT
+router.put("/update/:commentId/replies/:replyId",updateReplyCommentController);
 
 module.exports = router;
